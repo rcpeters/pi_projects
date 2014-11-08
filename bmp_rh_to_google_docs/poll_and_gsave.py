d3614827f18e595ea08c515f1c73961449a0b236
rcpeters <info@rcpeters.com>
2014-11-08 05:53:00 +0000

    rework to login into google more often

--- bmp_rh_to_google_docs/poll_and_gsave.py
+++ bmp_rh_to_google_docs/poll_and_gsave.py
@@ -1,51 +1,69 @@
-from ConfigParser import SafeConfigParser
-import gspread
-import bmp # Note this initialize the barometric sensor. See bmp.py
-import datetime
 import Adafruit_DHT
+import bmp # Note this initialize the barometric sensor. See bmp.py
+from ConfigParser import SafeConfigParser
+import datetime
+import gspread
+import logging
 import time
 
-BMP = 'BMP'
-RH = 'RH'
+
+
+# parse the config.ini
+parser = SafeConfigParser()
+parser.read('config.ini')
+g_email = parser.get('gspread', 'g_email')
+g_password = parser.get('gspread', 'g_password')
+g_spreadsheet = parser.get('gspread', 'g_spreadsheet')
+g_worksheet = parser.get('gspread', 'g_worksheet')
+sleep = eval(parser.get('polling', 'sleep'))
+error_sleep = eval(parser.get('polling', 'error_sleep'))
+
+def login_get_spread():
+	print 'google spreadsheet login'
+	return gspread.login(g_email, g_password).open(g_spreadsheet);
 
 def getIsoTime():
 	return datetime.datetime.utcnow().isoformat() + 'Z'
 
-def init_sheet(sprd, name):
-	if name == BMP:
-		col_names = ['Time ms', 'P Pa', 'P inH2O', 'P inHg', 'Sea-P Pa', 'Sea-P inH2O', 'Sea-P inHg', 'Alt M', 'Alt FT', 'Temp C', 'Temp F']
-	elif name == RH:
-		col_names = ['Time ms', 'RH ', 'Temp C', 'Temp F']
+def init_sheet(name):
+	sprd = login_get_spread()
+	col_names = ['Time ms', 'P Pa', 'P inH2O', 'P inHg'
+		, 'Sea-P Pa', 'Sea-P inH2O', 'Sea-P inHg'
+		, 'Alt M', 'Alt FT', 'Temp C(BMP)', 'Temp F(BMP)'
+                , 'RH ', 'Temp C(RH)', 'Temp F(RH)']
 	wrk = None
 	try:
-		wrk = sprd.worksheet(name)
+		wrk = sprd.worksheet(g_worksheet)
 	except gspread.exceptions.WorksheetNotFound:
 		print 'adding worksheet ' +  name 
-		sprd.add_worksheet(name,1,len(col_names))
+		sprd.add_worksheet(g_worksheet,1,len(col_names))
 	if wrk is None:
-		wrk = sprd.worksheet(name)
-	if wrk.cell(1, 1).value is None:
+		wrk = sprd.worksheet(g_worksheet)
+	print wrk.cell(1, 1).value
+	if wrk.cell(1, 1).value == '':
 		header_cells = wrk.range('A1:'+ wrk.get_addr_int(1, len(col_names)))
 		for i in range(len(col_names)):
 			header_cells[i].value = col_names[i]
 		wrk.update_cells(header_cells)
 	return wrk
 
-def add_bmp_row(r):
-	global bmpWrk
-	nextRow = bmpWrk.row_count + 1
-	vals = [getIsoTime()
-		, r.pressure
-		, '=B{0} * 0.0040147421331128'.format(nextRow)
-		, '=B{0} * 0.0002953337'.format(nextRow)
-		, r.sealevel_pressure
-		, '=E{0} * 0.0040147421331128'.format(nextRow)
-		, '=E{0} * 0.0002953337'.format(nextRow)
-		, r.altitude
-		, '=G{0} * 3.28084'.format(nextRow)
-		, r.temp
-		, '=J{0} * 1.8 + 32'.format(nextRow)]
-	bmpWrk.append_row(vals)
+def add_bmp_rh_row(wrk, r_bmp, r_rh):
+	nextRow = wrk.row_count + 1
+	vals = [ getIsoTime() #A
+		, r_bmp.pressure #B
+		, '=B{0} * 0.0040147421331128'.format(nextRow) #C
+		, '=B{0} * 0.0002953337'.format(nextRow) #D
+		, r_bmp.sealevel_pressure #E
+		, '=E{0} * 0.0040147421331128'.format(nextRow) #F
+		, '=E{0} * 0.0002953337'.format(nextRow) #G
+		, r_bmp.altitude #H
+		, '=H{0} * 3.28084'.format(nextRow) #I
+		, r_bmp.temp #J
+		, '=J{0} * 1.8 + 32'.format(nextRow) #K
+		, r_rh[0] #L
+		, r_rh[1] #M
+		, '=M{0} * 1.8 + 32'.format(nextRow)] #N
+	wrk.append_row(vals)
 
 def add_rh_row(r):
 	global rhWrk
@@ -56,69 +74,42 @@ def add_rh_row(r):
 
 def log_error_sleep(ex):
 	print "An exception {0} occured. Arguments:\n{1!r}".format(type(ex).__name__, ex.args)
-	print "retrying in 60 seconds"
-	time.sleep(60)
+	print "retrying in {0} seconds".format(error_sleep)
 
-# do to a tone of http errors with google
+	time.sleep(error_sleep)
+
+   
+# do to a ton of http errors with google
 # keep trying until all steps complete
-print 'starting settup'
 setup_success = False
 while setup_success == False:
 	try:
-		# parse the config.ini
-		parser = SafeConfigParser()
-		parser.read('config.ini')
-
-		# login to google and get the spreadsheet
-		print 'login'
-		gc = gspread.login(parser.get('gspread', 'gmail_email'), parser.get('gspread', 'gmail_password'))
-		sprd = gc.open(parser.get('gspread', 'gSpreadSheetName'))
-
-		# initalize the barometric spreedsheet tab
-		print 'init sheets'
-		bmpWrk = init_sheet(sprd, BMP)
-		rhWrk = init_sheet(sprd, RH)
-		
-		print 'clean Sheet1'
-		# Clear out Sheet1 if still there
-		try:
-			sheet1 = sprd.worksheet('Sheet1')
-			if sheet1 is not None:
-				sprd.del_worksheet(sheet1)
-		except gspread.exceptions.WorksheetNotFound:
-			pass
+		print 'init sheet'
+		init_sheet(g_worksheet)
 		setup_success = True
 	except Exception as ex:
 		log_error_sleep(ex)
 
-time.sleep(30)
 
 while True:
-	bmp_success = False
-	while bmp_success == False:
+	success = False
+	while success == False:
 		try:
+			sprd = login_get_spread()
+			wrk = sprd.worksheet(g_worksheet)
 			print 'bmp read'
-			r = bmp.getReading()
-			add_bmp_row(r)
-			print 'result {0}, {1}, {2}, {3}'.format(r.temp, r.altitude, r.pressure, r.sealevel_pressure) 
-			bmp_success = True
+			r_bmp = bmp.getReading()
+			print 'bmp result {0}, {1}, {2}, {3}'.format(r_bmp.temp, r_bmp.altitude, r_bmp.pressure, r_bmp.sealevel_pressure) 
+			r_rh = Adafruit_DHT.read_retry(22, 22)
+			print 'rh result {0}, {1}'.format(r_rh[0], r_rh[1]) 
+			add_bmp_rh_row(wrk, r_bmp, r_rh)
+			print 'saved worked'
+			success = True
 			print
 		except Exception as ex:
 			log_error_sleep(ex)
 	
-	rh_success = False
-	while rh_success == False:
-		try:
-			print 'rh read'
-			t = Adafruit_DHT.read_retry(22, 22)
-			add_rh_row(t)
-			print 'result {0}, {1}'.format(t[0], t[1])
-			rh_success = True
-			print
-		except Exception as ex:
-			log_error_sleep(ex)
-
-	print 'Sleeping for 10 mintues'
-	time.sleep(60 * 10)
+	print 'Sleeping for {0} seconds'.format(sleep)
+	time.sleep(sleep)
 	print
 	
