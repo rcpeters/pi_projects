665f5b0ea9827d47def41dfd21e98be9ac7e6320
rcpeters <info@rcpeters.com>
2014-11-15 17:02:56 +0000

    updates post w1thermsensor patches

--- bmp_rh_to_google_docs/poll_and_gsave.py
+++ bmp_rh_to_google_docs/poll_and_gsave.py
@@ -1,5 +1,5 @@
 import Adafruit_DHT
-import bmp # Note this initialize the barometric sensor. See bmp.py
+import Adafruit_BMP.BMP085
 from ConfigParser import SafeConfigParser
 import datetime
 import gspread
@@ -7,6 +7,9 @@ import logging
 import time
 
 RAW_SENSOR_SHEET_NAME = 'RAW SENSOR DATA'
+WEATHER_SHEET_NAME = 'Weather'
+
+bmp_sensor = Adafruit_BMP.BMP085.BMP085()
 
 # parse the config.ini
 parser = SafeConfigParser()
@@ -18,18 +21,22 @@ g_worksheet = parser.get('gspread', 'g_worksheet')
 sleep = eval(parser.get('polling', 'sleep'))
 error_sleep = eval(parser.get('polling', 'error_sleep'))
 
+
 def login_get_spread():
 	print 'google spreadsheet login'
 	return gspread.login(g_email, g_password).open(g_spreadsheet);
 
+
 def getIsoTime():
 	return datetime.datetime.utcnow().isoformat() + 'Z'
 
+
 def init_sheet(name):
-	sprd = login_get_spread()
-	col_names = ['Time ms', 'P Pa'
-		, 'Sea-P Pa', 'Alt M', 'Alt FT', 'Temp C(BMP)'
-                , 'RH ', 'Temp C(RH)']
+    sprd = login_get_spread()
+    if name == RAW_SENSOR_SHEET_NAME:
+        col_names = ['Time ms', 'Pa (BMP180)' , 'Temp C(BMP180)', 'RH(DHT22)', 'Temp C(DHT22)', 'TEMP C(DS18B20)']
+#    elif name = 
+#        col_names = ['Time ms', 'Pa Sea Level' , 'Temp C(BMP180)', 'RH(DHT22)', 'Temp C(DHT22)', 'TEMP C(DS18B20)']
 	wrk = None
 	try:
 		wrk = sprd.worksheet(name)
@@ -49,6 +56,11 @@ def init_sheet(name):
 		wrk.update_cells(header_cells)
 	return wrk
 
+
+def bmp_reading():
+    return bmp_sensor.read_pressure(), bmp_sensor.read_temperature()
+
+
 def add_raw_sensor_data(wrk, r_bmp, r_rh):
 	rows = wrk.row_count;
 	cols = wrk.col_count;
@@ -57,18 +69,16 @@ def add_raw_sensor_data(wrk, r_bmp, r_rh):
 		rows = rows+1
 	cells = wrk.range('{0}:{1}'.format(wrk.get_addr_int(rows, 1), wrk.get_addr_int(rows, cols)))
 	cells[0].value = getIsoTime() #A
-	cells[1].value = r_bmp.pressure #B
-	cells[2].value = r_bmp.sealevel_pressure #C
-	cells[3].value = r_bmp.altitude #D
-	cells[4].value = r_bmp.temp #E
-	cells[5].value = r_rh[0] #F
-	cells[6].value = r_rh[1] #G
+	cells[1].value = r_bmp[0]#B
+	cells[2].value = r_bmp[1] #C
+	cells[3].value = r_rh[0] #D
+	cells[4].value = r_rh[1] #E
 	wrk.update_cells(cells)
+
 
 def log_error_sleep(ex):
 	print "An exception {0} occured. Arguments:\n{1!r}".format(type(ex).__name__, ex.args)
 	print "retrying in {0} seconds".format(error_sleep)
-
 	time.sleep(error_sleep)
 
    
@@ -89,8 +99,8 @@ while True:
 	while success == False:
 		try:
 			print 'read sensors'
-			r_bmp = bmp.getReading()
-			print 'bmp result {0}, {1}, {2}, {3}'.format(r_bmp.temp, r_bmp.altitude, r_bmp.pressure, r_bmp.sealevel_pressure) 
+			r_bmp = bmp_reading()
+			print 'bmp result {0}, {1}'.format(r_bmp[0], r_bmp[1]) 
 			r_rh = Adafruit_DHT.read_retry(22, 22)
 			print 'rh result {0}, {1}'.format(r_rh[0], r_rh[1]) 
 			sensorWrk = login_get_spread().worksheet(RAW_SENSOR_SHEET_NAME)
--- digital_temp_DS18B20/poll_w_lib.py
+++ digital_temp_DS18B20/poll_w_lib.py
@@ -1,8 +1,5 @@
 import sys
 import os
-lib_path = os.path.abspath('w1thermsensor')
-sys.path.append(lib_path)
-
 from w1thermsensor import W1ThermSensor
 
 sensor = W1ThermSensor()
